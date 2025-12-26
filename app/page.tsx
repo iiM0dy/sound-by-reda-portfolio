79c335fbd7306166865e251b552f445a7f8cb8a1
Mohamed Khaled <106553775+iiM0dy@users.noreply.github.com>
2025-12-26 22:09:47 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div id="projects">
         <Projects />
       </div>
-      <div className="t-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-[#1A1A1A]" id="contact">
+      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-[#1A1A1A]" id="contact">
         <Footer />
       </div>
     </div>
