d02d25feea4bdcfc6ab1d7a5bb4066e59c3a3958
Mohamed Khaled <106553775+iiM0dy@users.noreply.github.com>
2026-01-07 01:15:29 +0200

    Add ProjectsPage component with video sections
    This file contains a React component for displaying projects, including movies, series, commercials, and shows, with their respective details and embedded video links.

---
+++ app/projects/page.tsx
@@ -0,0 +1,108 @@
+import React from 'react'
+
+interface Src {
+    id: number;
+    title: string;
+    description: string;
+    promoUrl: string;
+}
+
+const movies: Src[] = [
+    { id: 1, title: 'El Sett (Egyptian Movie)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/LlhGrxC5Ifc' },
+    { id: 2, title: 'Kira w El Ginn (Egyptian Movie)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/COkUlflnkbk' },
+    { id: 3, title: 'Shee’o (Egyptian Movie)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/bzOr9-9bj6g' },
+    { id: 4, title: 'Esabet Azema (Egyptian Movie)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/76XZybzJQ14' },
+    { id: 5, title: 'Baa’d El Shar (Egyptian Movie)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/9gnkWl8R5pI' },
+    { id: 6, title: 'Mr. EX (Egyptian Movie)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/p6FcQ00jTUQ' },
+];
+
+const series: Src[] = [
+    { id: 1, title: 'Al Hashasheen (Egyptian Series)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/DCO8fe0rOyE' },
+    { id: 2, title: 'Mamlakat El Harir (Egyptian Series)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/ziCJ-_c3o58' },
+    { id: 3, title: 'Ramadan Kareem Season 2 (Egyptian Series)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/onAJv3VNT2M' },
+    { id: 4, title: 'Ala Bab El Omara (Egyptian Series)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/s2LHCvfMgso' },
+    { id: 5, title: 'El Laaba Season 4 (Egyptian Series)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/ma7InmmiNi4' },
+    { id: 6, title: 'Hakeem Basha (Egyptian Series)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/KxLXLJC8y-0' },
+    { id: 7, title: 'Ahwet El Mahatta (Egyptian Series)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/aZaVihEuVTE' },
+    { id: 8, title: 'Gameela (Egyptian Series)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/rzgFBuHstuw' },
+    { id: 9, title: 'El Sofara (Egyptian Series)', description: 'participating On-Set Sound Recording', promoUrl: 'https://youtube.com/embed/ZJDBN2SHuJM' }];
+
+const commercials: Src[] = [
+
+    { id: 1, title: 'Etisalat – Sherine Abdel Wahab (Commercial)', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/dT3Z1272d-Y' },
+    { id: 2, title: 'Grand Egyptian Museum Opening (Event)', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/zYs_vKoeHGI' },
+    { id: 3, title: 'Grand Egyptian Museum Opening (Event)', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/CSl2cpee-DY?si=jDlc24cli4oCyjUI' },
+    { id: 4, title: 'Grand Egyptian Museum Opening (Event)', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/Xl73Upsiy6U?si=mkBHFyIOsEamaRaY' },
+    { id: 5, title: 'Grand Egyptian Museum Opening (Event)', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/DnKnlBdHxsU?si=g2aLHMektZfYkpzZ' },
+    { id: 6, title: 'Grand Egyptian Museum Opening (Event)', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/zKnSL1qESpo?si=WKrE6TZkshy87zuN' },
+    { id: 7, title: 'Madinaty 2025 (Commercial)', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/sHttFAKTHRU' },
+    { id: 8, title: 'Hadeed Ezz 2025 (Commercial)', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/mqxec0scFoo' },
+
+];
+
+const shows: Src[] = [
+    { id: 1, title: 'بودكاست الحل إيه؟ المسؤولية المجتمعية لرجال الأعمال في مصر مع رجل الأعمال سميح ساويرس', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/HWEPMqnu-TA?si=iLCzVrvWMVr7HQ0H' },
+    { id: 2, title: 'Demagh Sem - MSH KEDA', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/-TrIR6ule_w?si=AF3xb-V89asIHB4g' },
+    { id: 3, title: 'Demagh Sem - MSH KEDA', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/D7EQ0ImwjZc?si=aVO8vlIOlklD9G3Y' },
+    { id: 4, title: 'Demagh Sem - MSH KEDA', description: 'Production Sound Mixer', promoUrl: 'https://youtube.com/embed/oh-NMv2P89E?si=ZZDgQm4DGmVJwKni' },
+    { id: 5, title: 'بودكاست الحل إيه؟ حلقة خاصه عن البحث العلمي مع الدكتور محمد ابو الغار ورباب المهدي', description: 'As Production Sound Mixer', promoUrl: 'https://youtube.com/embed/YgB_9Md5YRg?si=0qbMCjASicp7Xeu-' },
+    { id: 6, title: 'بودكاست الحل إيه؟ | ماذا بعد 7 أكتوبر؟ .. عمرو موسى يطرح رؤيته للعالم العربي بعد السابع من أكتوبر', description: 'As Production Sound Mixer', promoUrl: 'https://youtube.com/embed/ZSRug1CN0e0?si=scJ4tublDgT6R9O9' },
+    { id: 7, title: 'المقايضة الكبرى | هل تكون الحل المثالي لديون مصر... رجل الأعمال حسن هيكل يعرض خطته لـ تصفير ديون مصر', description: 'As Production Sound Mixer', promoUrl: 'https://youtube.com/embed/MG0PlAXtq_g?si=GVpdgL2xtYpqk5wF' },
+    { id: 8, title: 'الفتنة الكبرى .. أخطر حقبة في التاريخ الإسلامي | في الحضارة', description: 'As Production Sound Mixer', promoUrl: 'https://youtube.com/embed/ldBgcsfJnU0?si=bGREFwHyE4z16qYt' },
+    { id: 9, title: 'نتنياهو.. الخائف الأبدي │ في الحضارة', description: 'As Production Sound Mixer', promoUrl: 'https://youtube.com/embed/qx8V78wTmVI?si=hjwnHdsHFr_DxNji' },
+    { id: 10, title: 'بيكاسو...العبقري الأخير │ في الحضارة', description: 'As Production Sound Mixer', promoUrl: 'https://youtube.com/embed/QqdeHoHueXk?si=r4A4MfNpew_Haz_-' },
+    { id: 11, title: 'أعقل مجنون في العالم │ في الحضارة', description: 'As Production Sound Mixer', promoUrl: 'https://youtube.com/embed/6oxwfDVVnVU?si=eFcAYXi5rrGY5pit' },
+    { id: 12, title: 'إمام ونجم.. في مواجهة العالم | في الحضارة', description: 'As Production Sound Mixer', promoUrl: 'https://youtube.com/embed/rCRFNbyzrc4?si=UEqart7xNfIEdg7V' },
+    { id: 13, title: 'أبو العلاء المعري: هل الاستسلام هو الخيار الأذكى أحيانًا؟ │ في الحضارة', description: 'As Production Sound Mixer', promoUrl: 'https://youtube.com/embed/TtW1OSalTXg?si=80lI9SGaM18AiGoH' },
+]
+
+const VideoCard = ({ item }: { item: Src }) => {
+    const isInstagram = item.promoUrl.includes("instagram.com");
+
+    return (
+        <div className="w-full md:w-140 px-4 md:px-0">
+            <div
+                className={`relative w-full overflow-hidden rounded-lg pb-[56.25%] `}
+            >
+                <iframe
+                    className="absolute inset-0 w-full h-full"
+                    src={item.promoUrl}
+                    title={item.title}
+                    frameBorder="0"
+                    scrolling="no"
+                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                />
+            </div>
+
+            <h2 className="text-white text-2xl mt-4">{item.title}</h2>
+            <p className="mt-2 text-[#A8A8A8]">{item.description}</p>
+        </div>
+    );
+};
+
+const Section = ({ title, data }: { title: string; data: Src[] }) => (
+    <>
+        <h1 className="text-4xl md:text-5xl font-bold text-secondery-color text-center py-10">{title}</h1>
+        <div className="flex flex-wrap gap-8 justify-center px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            {data.map(item => (
+                <VideoCard key={item.id} item={item} />
+            ))}
+        </div>
+    </>
+);
+
+const ProjectsPage = () => {
+    return (
+        <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+            <h1 className='text-5xl text-white font-bold text-center mt-10'>Projects</h1>
+            <p className='text-[#A8A8A8] mt-5 text-center'>We shape sound that breathes life into stories, from the raw energy of the set to the depth of the final mix</p>
+
+            <Section title="Movies" data={movies} />
+            <Section title="Series" data={series} />
+            <Section title="Commercials" data={commercials} />
+            <Section title="Shows" data={shows} />
+        </div>
+    )
+}
+
+export default ProjectsPage
