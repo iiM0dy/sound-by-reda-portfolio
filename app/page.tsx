c33eae2390d15cfd47b5eab97bfe2d3866482309
Mohamed Khaled <106553775+iiM0dy@users.noreply.github.com>
2026-01-06 12:21:02 +0200

    Add Introduce component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import Introduce from "./components/Introduce";
 
 export default function Home() {
   return (
     <div className="">
+      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <Introduce/>
+      </div>
       <div id="projects">
         <Projects />
       </div>
