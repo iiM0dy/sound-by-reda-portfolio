411951245482bb410b65b9b2d83bde939838b754
Mohamed Khaled <106553775+iiM0dy@users.noreply.github.com>
2026-01-07 01:12:41 +0200

    Update navigation links to use proper paths

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -9,9 +9,9 @@ const Menu = () => {
             {/* LARGE SCREENS */}
             <div className='hidden lg:flex'>
                 <Link href='/' className='px-4 text-xl text-white font-semibold before:content-[""] relative hover:before:absolute before:left-4 before:-bottom-1 before:w-0 hover:before:w-[calc(100%-32px)] before:h-0.5 before:bg-white before:duration-300 duration-300 before:ease-in-out ease-in-out'>Home</Link>
-                <Link href='/#projects' className='px-4 text-xl text-white font-semibold before:content-[""] relative hover:before:absolute before:left-4 before:-bottom-1 before:w-0 hover:before:w-[calc(100%-32px)] before:h-0.5 before:bg-white before:duration-300 duration-300 before:ease-in-out ease-in-out'>Projects</Link>
-                <Link href='/#contact' className='px-4 text-xl text-white font-semibold before:content-[""] relative hover:before:absolute before:left-4 before:-bottom-1 before:w-0 hover:before:w-[calc(100%-32px)] before:h-0.5 before:bg-white before:duration-300 duration-300 before:ease-in-out ease-in-out'>Contact</Link>
+                <Link href='/projects' className='px-4 text-xl text-white font-semibold before:content-[""] relative hover:before:absolute before:left-4 before:-bottom-1 before:w-0 hover:before:w-[calc(100%-32px)] before:h-0.5 before:bg-white before:duration-300 duration-300 before:ease-in-out ease-in-out'>Projects</Link>
                 <Link href='/bts' className='px-4 text-xl text-white font-semibold before:content-[""] relative hover:before:absolute before:left-4 before:-bottom-1 before:w-0 hover:before:w-[calc(100%-32px)] before:h-0.5 before:bg-white before:duration-300 duration-300 before:ease-in-out ease-in-out'>BTS</Link>
+                <Link href='/#contact' className='px-4 text-xl text-white font-semibold before:content-[""] relative hover:before:absolute before:left-4 before:-bottom-1 before:w-0 hover:before:w-[calc(100%-32px)] before:h-0.5 before:bg-white before:duration-300 duration-300 before:ease-in-out ease-in-out'>Contact</Link>
             </div>
             {/* SMALL SCREENS */}
             <div className='w-10 h-10  flex flex-col justify-around cursor-pointer lg:hidden' onClick={() => setIsOpen((prev) => !prev)}>
@@ -22,9 +22,9 @@ const Menu = () => {
             {isOpen && (
                 <div className='h-max w-full bg-[#1A1A1A] absolute top-20 right-0 flex flex-col items-center text-white text-lg font-semibold lg:hidden'>
                     <Link href='/' onClick={() => setIsOpen((prev) => !prev)}><p className='p-4'>Home</p></Link>
-                    <Link href='#projects' onClick={() => setIsOpen((prev) => !prev)}><p className='p-4'>Projects</p></Link>
-                    <Link href='#contact' onClick={() => setIsOpen((prev) => !prev)}><p className='p-4'>Contact</p></Link>
+                    <Link href='/projects' onClick={() => setIsOpen((prev) => !prev)}><p className='p-4'>Projects</p></Link>
                     <Link href='/bts' onClick={() => setIsOpen((prev) => !prev)}><p className='p-4'>BTS</p></Link>
+                    <Link href='#contact' onClick={() => setIsOpen((prev) => !prev)}><p className='p-4'>Contact</p></Link>
                 </div>
             )}
         </div>
