6d4d1f364bbb9b6befe40febcadf42cf6b7027f9
Mohamed Khaled <106553775+iiM0dy@users.noreply.github.com>
2026-01-07 01:13:49 +0200

    Update AboutSection.tsx

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -7,22 +7,19 @@ import { FaWhatsapp } from "react-icons/fa";
 const AboutSection = () => {
   return (
     <div className=''>
-      <div className='max-w-[420px] text-white '>
-        <p className='font-semibold mb-2 text-2xl'>About Me</p>
-        <p className='text-md text-[#A8A8A8]'>I am a sound-focused professional specializing in production sound
-        mixing and audio post-production for film, television, and digital
-        content. From on-set recording to final mix, I treat sound as a
-        storytelling tool, shaping emotion, rhythm, and realism with precision
-        and clarity. I work closely with directors, producers, and editors to
-        ensure every detail serves the narrative. My goal is clean, expressive,
-        cinematic sound that translates seamlessly across platforms and
-        cultures.</p>
+      <div className='max-w-105 text-white '>
+        <p className='font-semibold mb-2 text-2xl'>About Us</p>
+        <p className='text-md text-[#A8A8A8]'>
+          We are a sound-focused studio specializing in Production Sound Mixing and Audio Post-Production for film, television, and digital content.
+          From on-set recording to the final mix, we approach sound as a storytelling tool, not just a technical requirement. Our work is built on precision, clarity, and a deep understanding of how sound shapes emotion, rhythm, and realism.
+          We collaborate closely with directors, producers, and editors to ensure that every sonic detail serves the narrative—whether it’s dialogue, ambience, or sound design.
+          Our goal is simple: clean, expressive, and cinematic sound that translates seamlessly across platforms and cultures.</p>
       </div>
       <div className='text-white mt-12 mb-6'>
         <p className='text-lg font-semibold'>Contacts</p>
         <Link href='mailto:' className='underline'>contact@soundbyreda.com</Link>
       </div>
-      
+
       <div className='flex gap-4'>
         <Link target='_blank' href='https://www.instagram.com/soundbyreda/'><FaInstagram className='cursor-pointer text-3xl hover:text-[#A8A8A8] text-white ease-in-out duration-300' /></Link>
         <Link target='_blank' href='https://wa.me/201025811491'><FaWhatsapp className='cursor-pointer text-3xl hover:text-[#A8A8A8] text-white ease-in-out duration-300' /></Link>
