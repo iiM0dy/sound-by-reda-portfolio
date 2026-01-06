8f66f04e8e135268dbcdb7db89b1f829e2a07b2d
Mohamed Khaled <106553775+iiM0dy@users.noreply.github.com>
2026-01-07 01:11:35 +0200

    Add Services component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,21 @@ import Image from "next/image";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import Introduce from "./components/Introduce";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
     <div className="">
       <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <Introduce/>
+        <Introduce />
+      </div>
+      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <Services />
       </div>
       <div id="projects">
         <Projects />
       </div>
-      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-[#1A1A1A]" id="contact">
-        <Footer />
-      </div>
+
     </div>
   );
 }
