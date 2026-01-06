2f88d32f088e59c4555dea68a0735cb234aa9e07
Mohamed Khaled <106553775+iiM0dy@users.noreply.github.com>
2026-01-07 01:12:26 +0200

    Fix Navbar component formatting and structure

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,10 +5,10 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-[#1A1A1A] sticky top-0 z-50' id='home'>
-        <div className='h-full flex items-center justify-between'>
+      <div className='h-full flex items-center justify-between'>
         <Link href='/'><Image src='/logo.png' alt='Logo' width={80} height={80} /></Link>
-        <Menu/>
-        </div>
+        <Menu />
+      </div>
     </div>
   )
 }
