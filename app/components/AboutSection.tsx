fef0b2a3d88935f93b49bf76e726b333699cfbcf
Mohamed Khaled <106553775+iiM0dy@users.noreply.github.com>
2026-01-06 12:22:24 +0200

    Enhance AboutSection with detailed description and links
    Updated the AboutSection component to provide a more detailed description of the author's professional background and changed the links to open in a new tab.

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -8,17 +8,25 @@ const AboutSection = () => {
   return (
     <div className=''>
       <div className='max-w-[420px] text-white '>
-        <p className='font-semibold text-2xl'>About Me</p>
-        <p className='text-md text-[#A8A8A8]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium necessitatibus laborum ipsam obcaecati dolor molestiae illo eum culpa animi, quisquam, explicabo facilis ducimus, vero quidem sunt tempora expedita quos? Amet.</p>
+        <p className='font-semibold mb-2 text-2xl'>About Me</p>
+        <p className='text-md text-[#A8A8A8]'>I am a sound-focused professional specializing in production sound
+        mixing and audio post-production for film, television, and digital
+        content. From on-set recording to final mix, I treat sound as a
+        storytelling tool, shaping emotion, rhythm, and realism with precision
+        and clarity. I work closely with directors, producers, and editors to
+        ensure every detail serves the narrative. My goal is clean, expressive,
+        cinematic sound that translates seamlessly across platforms and
+        cultures.</p>
       </div>
       <div className='text-white mt-12 mb-6'>
         <p className='text-lg font-semibold'>Contacts</p>
         <Link href='mailto:' className='underline'>contact@soundbyreda.com</Link>
       </div>
+      
       <div className='flex gap-4'>
-        <Link href='https://www.instagram.com/soundbyreda/'><FaInstagram className='cursor-pointer text-3xl hover:text-[#A8A8A8] text-white ease-in-out duration-300' /></Link>
-        <Link href='https://wa.me/201025811491'><FaWhatsapp className='cursor-pointer text-3xl hover:text-[#A8A8A8] text-white ease-in-out duration-300' /></Link>
-        <Link href='mailto:contact@soundbyreda.com'><MdEmail className='cursor-pointer text-3xl hover:text-[#A8A8A8] text-white ease-in-out duration-300' /></Link>
+        <Link target='_blank' href='https://www.instagram.com/soundbyreda/'><FaInstagram className='cursor-pointer text-3xl hover:text-[#A8A8A8] text-white ease-in-out duration-300' /></Link>
+        <Link target='_blank' href='https://wa.me/201025811491'><FaWhatsapp className='cursor-pointer text-3xl hover:text-[#A8A8A8] text-white ease-in-out duration-300' /></Link>
+        <Link target='_blank' href='mailto:contact@soundbyreda.com'><MdEmail className='cursor-pointer text-3xl hover:text-[#A8A8A8] text-white ease-in-out duration-300' /></Link>
 
       </div>
     </div>
